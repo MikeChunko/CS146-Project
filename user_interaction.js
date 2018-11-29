7f404537aff16ddfc36ef3bc60680042b8f94955
Mike Chunko <chunkomike@gmail.com>
2018-11-29 14:38:30 -0500

    Super minor update to user_interaction.js

--- user_interaction.js
+++ user_interaction.js
@@ -12,12 +12,12 @@ window.onload = () => {
 
         img_list[i].addEventListener("mouseover", function () {
             img_list[i].setAttribute("style", "border-radius: 5%;" + "border: 5px solid red");
-            img_list[i].style.transition = "all 1s";
+            img_list[i].style.transition = "all 1s ease";
         });
 
         img_list[i].addEventListener("mouseleave", function () {
             img_list[i].style = original_properties[i];
-            img_list[i].style.transition = "all 1s";
+            img_list[i].style.transition = "all 1s ease";
         });
     }
 };
